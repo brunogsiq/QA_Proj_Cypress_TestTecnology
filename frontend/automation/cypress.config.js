e5d14f645c617768a9d546d7a0787e83f143f769
brunogsiq <brunogsiq@gmail.com>
2025-10-06 12:02:58 -0300

    novos files

--- frontend/automation/cypress.config.js
+++ frontend/automation/cypress.config.js
@@ -1,29 +1,36 @@
+//"https://www.textecnologia.com.br/testes/qa",
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  viewportWidth: 1300,
-  viewportHeight: 800,
-    //Habilitando downloads
-  //downloadsFolder: true,
-    //Habilitando screenshot
-  //screenshotOnRunFailure: true,
-  //screenshotsFolder: true,
-  //Habilitando videos
-  video: true,
-  //videoCompression: 0,
-  //videosFolder: true,
-    //Deletando tudo que foi gerado
-  //trashAssetsBeforeRuns: true,
-  env: {
-    baseUrl: "https://www.textecnologia.com.br/testes/qa",
-  },
-  e2e: {
-    experimentalRunAllSpecs: true,
-    //Aqui ficarão todos os meus testes:
-    specPattern: [
-       "cypress/tests/scenes/01 - Validar.cy.js",
-      // "cypress/tests/scenes/",
-    ],
-  },
-});
+	viewportWidth: 1300,
+	viewportHeight: 800,
+	video: true,
+	chromeWebSecurity: false,
 
+	reporter: "cypress-mochawesome-reporter",
+	reporterOptions: {
+		reportDir: "cypress/reports/mochawesome",
+		overwrite: true,
+		html: true,
+		json: false,
+		timestamp: "dd-mm-yyyy_HH-MM-ss",
+	},
+
+	e2e: {
+		baseUrl: "https://www.google.com.br",
+		defaultCommandTimeout: 9000,
+		experimentalRunAllSpecs: true,
+		hideXHRInCommandLog: true,
+		specPattern: "cypress/tests/scenes/**/*.cy.js",
+			setupNodeEvents(on, config) {
+				require("cypress-mochawesome-reporter/plugin")(on);
+			},
+	},
+
+	env: {
+		dev: "https://www.seuambiente-dev.com",
+		tst: "https://www.seuambiente-tst.com",
+		hml: "https://www.seuambiente-hml.com",
+		prod: "https://www.seuambiente-prod.com",
+	},
+});
